28b0f4a1830a3e44edea9181cd01cf4bdb860d31
hshchehelskyi <shchehelskyi.hlieb@gmail.com>
2022-01-18 15:38:39 +0200

    Updates

---
+++ precache-manifest.486891c027db88076ca3eadfe43c38f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "783d63a1b6f26176b48d",
+    "url": "/shop-project-react/static/css/main.f35afe21.chunk.css"
+  },
+  {
+    "revision": "783d63a1b6f26176b48d",
+    "url": "/shop-project-react/static/js/main.783d63a1.chunk.js"
+  },
+  {
+    "revision": "324ef8eaaf42cb5acf84",
+    "url": "/shop-project-react/static/js/1.324ef8ea.chunk.js"
+  },
+  {
+    "revision": "24a29c62572fbf35e9e6",
+    "url": "/shop-project-react/static/js/runtime~main.24a29c62.js"
+  },
+  {
+    "revision": "2a98c91c721d2fcc82e806e6d5897f28",
+    "url": "/shop-project-react/index.html"
+  }
+];
